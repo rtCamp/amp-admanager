5dcd13740b59a04ec0ea10c7ea6dc12beafa14b4
Pradeep Sonawane <pradeep.sonawane@rtcamp.com>
2019-04-10 13:18:10 +0530

    Update plugin version

--- amp-admanager.php
+++ amp-admanager.php
@@ -6,7 +6,7 @@
  * Plugin Name:  AMP AdManager
  * Plugin URI:   https://github.com/rtCamp/amp-admanager
  * Description:  AMP ads for all WordPress sites (AMP and Non-AMP)
- * Version:      1.0.0
+ * Version:      0.1
  * Author:       rtCamp
  * Author URI:   https://rtcamp.com
  * Text Domain:  amp-admanager
@@ -14,7 +14,7 @@
  * @package AMP_AdManager
  */
 
-define( 'AMP_ADMANAGER_VERSION', '1.0.0' );
+define( 'AMP_ADMANAGER_VERSION', '0.1' );
 define( 'AMP_ADMANAGER_ROOT', __DIR__ );
 
 require_once AMP_ADMANAGER_ROOT . '/classes/class-admin.php';
