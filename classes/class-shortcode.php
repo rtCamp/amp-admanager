1cbc81cbae979237cf7c42fa1b0b7e9cd0adca85
Pradeep Sonawane <pradeep.sonawane@rtcamp.com>
2019-04-14 23:35:39 +0530

    Add custom targeting support and doc

--- classes/class-shortcode.php
+++ classes/class-shortcode.php
@@ -2,7 +2,7 @@
 /**
  * AMP AdManager Shortcode Class.
  *
- * @author Vishal Dodiya <vishal.dodiya@rtcamp.com>
+ * @author  Vishal Dodiya <vishal.dodiya@rtcamp.com>
  *
  * @package AMP_AdManager
  */
@@ -29,21 +29,49 @@ class Shortcode {
 	 *
 	 * @return string
 	 */
-	public function render_amp_ad( $atts = [], $content = '' ) {
+	public function render_amp_ad( $attr = [], $content = '' ) {
 
 		$default_attr = [
-			'width'   => '300',
-			'height'  => '250',
-			'ad-unit' => '',
-			'min'     => '',
-			'max'     => '',
-			'sizes'   => '300x250,300x100',
-			'layout'  => 'responsive'
+			'width'            => '300',
+			'height'           => '250',
+			'ad-unit'          => '',
+			'min'              => '',
+			'max'              => '',
+			'sizes'            => '300x250,300x100',
+			'layout'           => 'responsive',
+			'custom-targeting' => ''
 		];
 
-		$atts = shortcode_atts( $default_attr, $atts );
+		$attr = shortcode_atts( $default_attr, $attr );
 
-		$ad_html = AMP_AdManager::get_amp_ad( $atts );
+		/**
+		 * Custom targeting for a specific amp-ad tag.
+		 *
+		 * Custom/User provided targeting added in shortcode to merged with default targeting array.
+		 * Ex. custom-targeting="key1:value1, key2:value2".
+		 *
+		 * @since 0.2
+		 */
+		if ( isset( $attr['custom-targeting'] ) && ! empty ( $attr['custom-targeting'] ) ) {
+
+			// Separate out all key values in array.
+			$custom_targeting = explode( ',', trim( $attr['custom-targeting'] ) );
+
+			if ( is_array( $custom_targeting ) && ! empty ( $custom_targeting ) ) {
+
+				foreach ( $custom_targeting as $value ) {
+
+					// Separate out individual targeting key values as $key => $value pair.
+					$new_key_value = explode( ':', trim( $value ) );
+
+					if ( ! empty ( $new_key_value ) ) {
+						$attr['targeting'][ trim( $new_key_value[0] ) ] = trim( $new_key_value[1] );
+					}
+				}
+			}
+		}
+
+		$ad_html = AMP_AdManager::get_amp_ad( $attr );
 
 		if ( empty( $ad_html ) ) {
 			return $content;
